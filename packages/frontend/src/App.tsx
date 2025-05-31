7559ad46ebc4931633e49a8af403cc3246fa8933
Kai Stout <kaistout4@gmail.com>
2025-05-30 21:05:27 -0700

    Lab18

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,17 +1,25 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router";
+import { MainLayout } from "./MainLayout.tsx";
 import { AllImages } from "./images/AllImages.tsx";
 import { ImageDetails } from "./images/ImageDetails.tsx";
 import { UploadPage } from "./UploadPage.tsx";
 import { LoginPage } from "./LoginPage.tsx";
+import { fetchDataFromServer } from "./MockAppData.ts";
 
 function App() {
-    const POSSIBLE_PAGES = [
-        <AllImages />,
-        <ImageDetails imageId={"0"} />,
-        <UploadPage />,
-        <LoginPage />
-    ];
-
-    return POSSIBLE_PAGES[0];
+    const [imageData, _setImageData] = useState(fetchDataFromServer);
+    
+    return (
+        <Routes>
+            <Route path="/" element={<MainLayout />}>
+                <Route index element={<AllImages imageData={imageData} />} />
+                <Route path="images/:imageId" element={<ImageDetails imageData={imageData} />} />
+                <Route path="upload" element={<UploadPage />} />
+                <Route path="login" element={<LoginPage />} />
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
